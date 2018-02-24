234566232463f55320c40161212246d3e1da7f2b
Jose Navarro Alabarta <joshevers86@gmail.com>
2018-02-24 13:53:46 +0100

    Initial Commit

---
+++ ProyectoFinal1Tests/ProyectoFinal1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ProyectoFinal1Tests.swift
+//  ProyectoFinal1Tests
+//
+//  Created by Jose Navarro Alabarta on 24/2/18.
+//  Copyright © 2018 Jose Navarro Alabarta. All rights reserved.
+//
+
+import XCTest
+@testable import ProyectoFinal1
+
+class ProyectoFinal1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
